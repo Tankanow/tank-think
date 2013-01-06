4cd3cff054205dd07777a51b0e557087b505cc6f
Adam Tankanow <Adam.Tankanow@gmail.com>
2013-01-05 22:30:11 -0500

    Filled in index-page view with some boiler plate html5 jooblog stuff.

--- src/compojure/example/views.clj
+++ src/compojure/example/views.clj
@@ -4,7 +4,16 @@
 (defn index-page []
   (html5
     [:head
-      [:title "Hello World"]
+      [:title "Tank Think"]
       (include-css "/css/style.css")]
     [:body
-      [:h1 "Hello World"]]))
+      [:header 
+      [:h1 "Tank Think"]]
+      [:section
+        [:header
+          [:h1 "Blog Posts"]]
+        [:article
+          [:header
+            [:h1 "Post 1"]]
+          [:p "This is my first post"]]]]
+      ))
