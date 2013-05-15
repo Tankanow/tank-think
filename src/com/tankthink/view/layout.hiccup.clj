2f76ed97291f392ea5c6355977edba9f7e440a8b
Adam Tankanow <Adam.Tankanow@gmail.com>
2013-05-15 11:55:18 -0400

    removed unncessary loading of missing javascript file; changed blog post title

--- src/com/tankthink/view/layout.hiccup.clj
+++ src/com/tankthink/view/layout.hiccup.clj
@@ -2,9 +2,8 @@
 [:html
  [:head
   [:meta {:http-equiv "Content-Type" :content "text/html" :charset "iso-8859-1"}]
-  [:title "jooblog"]
-  (include-css "/css/style.css")
-  (include-js "/javascript/jooblog.js")]
+  [:title "Tank Thinks ..."]
+  (include-css "/css/style.css")]
  [:body
   (eval (:template-body joodo.views/*view-context*))
 ]]
